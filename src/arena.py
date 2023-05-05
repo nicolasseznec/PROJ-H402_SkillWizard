dc4abe53dd0b4b71ca473e320c0c260614573008
Nicolas Seznec <seznec.nicolas@yahoo.fr>
2023-05-05 16:32:59 +0200

    Added Start Area PropertiesÃ

--- src/arena.py
+++ src/arena.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import math
 
-from PyQt5.QtWidgets import QGroupBox, QFrame, QGraphicsScene, QGraphicsItem, QGraphicsEllipseItem, QGraphicsView, \
+from PyQt5.QtWidgets import QGroupBox, QGraphicsScene, QGraphicsItem, QGraphicsEllipseItem, QGraphicsView, \
     QGraphicsPathItem
 from PyQt5.QtGui import QPainter, QColor, QPainterPath, QPolygonF, QPen, QBrush
 from PyQt5.QtCore import Qt, QPoint, QPointF
@@ -9,29 +9,103 @@ from PyQt5.QtCore import Qt, QPoint, QPointF
 from src.util import ResourceLoader, Event
 
 
-class ArenaShape(Enum):
+class Shape(Enum):
     Square = 1
     Hexagon = 2
     Octagon = 3
     Dodecagon = 4
     Circle = 5
+    Rectangle = 6
+
+
+ArenaShape = [Shape(i) for i in range(1, 6)]
+StartShape = [Shape.Circle, Shape.Rectangle]
 
 
 # Holds the parameters values
 class Arena:
     def __init__(self):
-        self.shape = ArenaShape.Square
+        self.shape = Shape.Square
         # self.sideLength = 66
 
 
-class MyEllipseItem(QGraphicsEllipseItem):
-    def itemChange(self, change, value):
+class StartArea(QGraphicsPathItem):
+    def __init__(self, arenaPath, *__args):
+        super().__init__(*__args)
+        self.setBrush(QBrush(QColor(224, 126, 134, 255)))
+        pen = QPen(QColor(Qt.black))
+        pen.setWidth(3)
+        pen.setStyle(Qt.DashDotLine)
+        self.setPen(pen)
 
-        # if change == QGraphicsItem.ItemPositionChange:
-        # print("New position:", self.scenePos().x(), self.scenePos().y())
-        #     print("New position:", value)
+        self.radius = 50
+        self.width = 100
+        self.height = 100
+        self.setShape(Shape.Circle)
+        self.updateDimensions()
 
-        return super().itemChange(change, value)
+        self.arenaPath = arenaPath
+
+    def paint(self, painter, option, widget=None):
+        intersect = self.arenaPath.intersected(self.shapePaths[self.shape].translated(self.scenePos())) \
+            .translated(-self.scenePos())
+        intersect.closeSubpath()
+        self.setPath(intersect)
+        super(StartArea, self).paint(painter, option, widget)
+
+    def getShapePath(self, shape):
+        path = QPainterPath()
+        if shape == Shape.Rectangle:
+            path.addRect(-self.width/2, -self.height/2, self.width, self.height)
+        elif shape == Shape.Circle:
+            path.addEllipse(-self.radius, -self.radius, self.radius*2, self.radius*2)
+        return path
+
+    def setShape(self, shape):
+        self.shape = shape
+
+    def setPosition(self, pos):
+        self.setPos(pos)
+
+    def setTabFocus(self, focus):
+        self.setOpacity(1.0 if focus else 0.4)
+
+    def connectSettings(self, container):
+        container.StartAreaShape.currentIndexChanged.connect(self.startAreaShapeChanged)
+        container.StartAreaReset.clicked.connect(self.resetStartArea)
+
+        container.StartAreaRadius.valueChanged.connect(self.radiusChanged)
+        container.StartAreaWidth.valueChanged.connect(self.widthChanged)
+        container.StartAreaHeight.valueChanged.connect(self.heightChanged)
+
+    def startAreaShapeChanged(self, index):
+        self.setShape(StartShape[index])
+        self.scene().update()
+
+    def resetStartArea(self):
+        self.setPosition(self.scene().center)
+
+    def radiusChanged(self, value):
+        self.radius = value
+        self.updateDimensions(Shape.Circle)
+
+    def widthChanged(self, value):
+        self.width = value
+        self.updateDimensions(Shape.Rectangle)
+
+    def heightChanged(self, value):
+        self.height = value
+        self.updateDimensions(Shape.Rectangle)
+
+    def updateDimensions(self, shape=None):
+        if shape is not None:
+            self.shapePaths[shape] = self.getShapePath(shape)
+        else:
+            self.shapePaths = {s: self.getShapePath(s) for s in StartShape}  # QGraphicsPathItem
+
+        scene = self.scene()
+        if scene is not None:
+            scene.update()
 
 
 class ArenaRenderArea(QGraphicsScene):    # Handles Arena graphics
@@ -39,38 +113,29 @@ class ArenaRenderArea(QGraphicsScene):    # Handles Arena graphics
         super().__init__(*__args)
         self.areaSize = QPoint(500, 500)
         self.center = QPointF(0, 0)
-        self.shape = ArenaShape.Dodecagon
+        self.shape = Shape.Dodecagon
         self.setSceneRect(-250, -250, 500, 500)
 
         self.shapePaths = {shape: self.getShapePath(shape) for shape in ArenaShape}  # QGraphicsPathItem
         self.initShapes()
 
-        # Add some items to the scene
-        # pen = QPen()
-        # pen.setWidth(3)
-        # self.ellipse_item: QGraphicsItem = self.addEllipse(50, 50, 200, 100, pen, QBrush(QColor(255, 255, 0)))
-        # self.ellipse_item: QGraphicsItem = MyEllipseItem(0, 0, 200, 100)
-        # self.ellipse_item.setPen(pen)
-        # self.ellipse_item.setBrush(QBrush(QColor(0, 0, 255)))
-        # self.ellipse_item.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsSelectable)
-        # self.addItem(self.ellipse_item)
-
-        # line_item = self.addLine(0, 0, 500, 500, pen)
-        # line_item.setPen(QPen(QColor(255, 0, 0), 5))
+        self.startArea = StartArea(self.shapePaths[self.shape].path())
+        self.startArea.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsSelectable)
+        self.addItem(self.startArea)
 
     def getShapePath(self, shape):
         path = QPainterPath()
 
-        if shape == ArenaShape.Square:
+        if shape == Shape.Square:
             path.addRect(-240, -240, 480, 480)
-        elif shape == ArenaShape.Circle:
+        elif shape == Shape.Circle:
             path.addEllipse(self.center, 240, 240)
         else:
-            if shape == ArenaShape.Hexagon:
+            if shape == Shape.Hexagon:
                 n = 6
-            elif shape == ArenaShape.Octagon:
+            elif shape == Shape.Octagon:
                 n = 8
-            elif shape == ArenaShape.Dodecagon:
+            elif shape == Shape.Dodecagon:
                 n = 12
             else:
                 n = 3
@@ -78,10 +143,11 @@ class ArenaRenderArea(QGraphicsScene):    # Handles Arena graphics
             angle = 360/n
             radius = self.areaSize.x()//2
             points = []
+            offset = angle/2 if shape != Shape.Hexagon else 0
 
             for i in range(n):
-                x = self.center.x() + radius * math.cos(math.radians(angle * (i + 0.5)))
-                y = self.center.y() + radius * math.sin(math.radians(angle * (i + 0.5)))
+                x = self.center.x() + radius * math.cos(math.radians(angle * i + offset))
+                y = self.center.y() + radius * math.sin(math.radians(angle * i + offset))
                 points.append(QPoint(int(x), int(y)))
             points.append(points[0])
 
@@ -101,8 +167,16 @@ class ArenaRenderArea(QGraphicsScene):    # Handles Arena graphics
     def setShape(self, shape):
         self.shapePaths[self.shape].setVisible(False)
         self.shapePaths[shape].setVisible(True)
+        self.startArea.arenaPath = self.shapePaths[shape].path()
         self.shape = shape
         self.update()
+
+    def connectSettings(self, container):
+        self.startArea.connectSettings(container)
+        container.ArenaEditSettings.currentChanged.connect(self.onTabChange)
+
+    def onTabChange(self, index):
+        self.startArea.setTabFocus(index == 0)
 
 
 class ArenaView(QGroupBox):
@@ -118,6 +192,7 @@ class ArenaView(QGroupBox):
         self.onArenaClicked = Event()
 
         self.arenaRenderArea = ArenaRenderArea()
+        self.arenaRenderArea.connectSettings(self)
         self.graphicsView.setScene(self.arenaRenderArea)
         self.graphicsView.setDragMode(QGraphicsView.NoDrag)
         self.graphicsView.setRenderHint(QPainter.Antialiasing)
@@ -129,7 +204,7 @@ class ArenaView(QGroupBox):
         self.onArenaClicked()
 
     def shapeChanged(self, index):
-        newShape = ArenaShape(index+1)
+        newShape = ArenaShape[index]
         # TODO : notify controller to update Arena model
         self.arenaRenderArea.setShape(newShape)
 
